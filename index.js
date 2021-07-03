4030523abf4c15e0ce5cb9add6d46d89b7a3d60d
Christian <nshimyumukizachristian>
2021-07-03 04:17:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    // const section = document.getElementById('cards')
+    // section.appendChild(data({ name: "Okk", email: "oko" }))
+    // console.log('====================================');
+    // console.log(section);
+    // console.log('====================================');
+    fetchData();
+})
+
+function fetchData() {
+    const section = document.getElementById('cards')
+
+    fetch('https://jsonplaceholder.typicode.com/users',).
+        then(data => data.json()).
+        then(data => {
+            data.forEach((data) => {
+                const newCard = card(data)
+                section.appendChild(newCard)
+            })
+        }).catch(e => console.log(e))
+}
+
+
+function card(data) {
+
+    const div = document.createElement('div')
+    div.className = 'card'
+
+    const header = document.createElement('span')
+    header.className = 'header'
+    header.innerText = data.name.slice(0, 2)
+
+    const name = document.createElement('span')
+    name.className = 'name'
+    name.innerText = data.name
+
+    const email = document.createElement('span')
+    email.className = 'email'
+    email.innerText = data.email
+
+    const btn = document.createElement('span')
+    btn.className = 'btn'
+    btn.innerText = 'Read User\'s Blogs'
+
+    div.appendChild(header)
+    div.appendChild(name)
+    div.appendChild(email)
+    div.appendChild(btn)
+
+    btn.addEventListener('click', function (e) {
+        document.getElementById('cards').style.display = 'none'
+        document.getElementById('header').style.display = 'none'
+    })
+    return div;
+}
+
